7c0eaa22b805cbfd21cad588114307b7d1c02c31
Wellington Correia Borsato Jr. <wellington.borsato@yahoo.com.br>
2024-10-19 00:49:12 +0000

    atualização do pom e do docker

--- src/main/java/com/example/BrownCoffee/controller/ProductController.java
+++ src/main/java/com/example/BrownCoffee/controller/ProductController.java
@@ -5,7 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
+
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,7 +22,7 @@ import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/product")
-@CrossOrigin(origins = "http://localhost:4200")
+
 
 public class ProductController {
 	@Autowired
